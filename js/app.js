298c8f36163e0c651a5f3eb65c72f9cefe777001
MohannadAteyyat <ateyyatmohanad@gmail.com>
2021-04-27 22:07:13 +0300

    Class02

--- js/app.js
+++ js/app.js
@@ -1,84 +1,218 @@
 ' use strict';
-let userName = prompt('Hello, could you please type your name?')
-console.log('uSERnAME :'+ userName)
+/*alert('Hello we are going to play a gues game')
 
-alert('Welcome to my humble page'+' '+ userName +' '+ 'please answer the following questions')
+let myOld = Number(prompt('How old I\'m I ?'))
 
-let qus1 = prompt( 'Q1-Are you a software developer?')
-console.log('Are you a software developer? :'+ qus1)
-switch (qus1.toLowerCase()) {
-    case 'yes':
-        document.write('<h1>' + 'Q1-Are you a software developer?' + '  ' + 'yes'+'</h1>')
-        break;
-    case 'no':
-        document.write('<h1>' + 'Q1-Are you a software developer?' + '  ' + 'no'+'</h1>')
-        break;
-    default:
-        document.write('<h1>' + 'Q1-'+'Sorry' + ' ' + userName + '  ' + ' , you did not answer well, please only answer with yes or no')
-        alert('please only answer with yes or no')
-        break;
+
+
+
+
+for (let i = 0; i < 3; i++) {
+
+    while (!myOld) { alert('Please Enter a number'), myOld = Number(prompt('How old I\'m I ?, writ only numbers'))
+    ,console.log('How old I\'m I ?' + myOld) }
+
+    if (myOld < 20) {
+        alert('Too Low')
+        myOld = Number(prompt('How old I\'m I ?'))
+        console.log('How old I\'m I ?' + myOld)
+    } else if (myOld > 35) {
+        alert('Too High')
+        myOld = Number(prompt('How old I\'m I ?'))
+        console.log('How old I\'m I ?' + myOld)
+    }
+    else if (myOld == 29) { alert('Great this is the correct answer') 
+    console.log('How old I\'m I ?' + myOld)}
+    else {
+        myOld = prompt('No wrong try one more time');
+
+    }
 }
+console.log('How old I\'m I ?' + myOld)
+alert('My old is 29, whish me a happy life')
 
-let qus2 = prompt('Q2-Are you looking for software developer?');
-console.log('Are you looking for software developer? :'+ qus2)
-switch (qus2.toLowerCase()) {
-    case 'yes':
-        document.write('<h1>' + 'Q2-Are you looking for software developer?' + '  ' + 'yes'+'</h1>')
-        break;
-    case 'no':
-        document.write('<h1>' + 'Q2-Are you looking for software developer?' + '  ' + 'no'+'</h1>')
-        break;
-    default:
-        document.write('<h1>' + 'Q2-'+'Sorry' + ' ' + userName + '  ' + ' , you did not answer well, please only answer with yes or no')
-        alert('please only answer with yes or no')
-        break;
+
+
+
+let birthYear=Number (prompt('What year I was porn?'))
+console.log('How old I\'m I ?' + birthYear)
+
+
+for (let i = 0; i < 3; i++) {
+
+    while (!birthYear) { alert('Please Enter a number'), birthYear = Number(prompt('What year I was porn?, writ only numbers'))
+    ,console.log('What year I was porn?' + birthYear) }
+
+    if (birthYear < 1985) {
+        alert('Too old for that')
+        birthYear = Number(prompt('What year I was porn?'))
+        console.log('What year I was porn?' + birthYear)
+    } else if (birthYear > 1997) {
+        alert('Too young for that')
+        birthYear = Number(prompt('What year I was porn?'))
+        console.log('What year I was porn?' + birthYear)
+    }
+    else if (birthYear == 1993) { alert('Great this is the correct answer') 
+    console.log('What year I was porn?' + birthYear)}
+    else {
+        birthYear = prompt('No wrong try one more time');
+
+    }
 }
+console.log('How old I\'m I ?' + myOld)
+alert('I porned in 1993, whish me a happy life')
 
-let qus3 = prompt('Q3-Did you liked my website?');
-console.log('Did you liked my website? :'+ qus3)
-switch (qus3.toLowerCase()) {
 
-    case 'yes':
-        document.write('<h1>' + 'Q3-Did you liked my website?' + '  ' + 'yes'+'</h1>')
-        break;
-    case 'no':
-        document.write('<h1>' + 'Q3-Did you liked my website?' + '  ' + 'no'+'</h1>')
-        break;
-    default:
-        document.write('<h1>' + 'Q3-'+'Sorry' + ' ' + userName + '  ' + ' , you did not answer well, please only answer with yes or no')
-        alert('please only answer with yes or no')
-        break;
+
+let myGrade=Number (prompt('what is my high school grade?'))
+console.log('what is my high school grade?' + myGrade)
+
+
+for (let i = 0; i < 3; i++) {
+
+    while (!myGrade) { alert('Please Enter a number'), myGrade = Number(prompt('what is my high school grade?, write only numbers'))
+    ,console.log('what is my high school grade?' + myGrade) }
+
+    if (myGrade < 50) {
+        alert('Too low for me')
+        myGrade = Number(prompt('what is my high school grade?'))
+        console.log('what is my high school grade?' + myGrade)
+    } else if (myGrade > 95) {
+        alert('More than what I desirve')
+        myGrade = Number(prompt('what is my high school grade?'))
+        console.log('what is my high school grade?' + myGrade)
+    }
+    else if (myGrade == 85) { alert('Great this is the correct answer') 
+    console.log('what is my high school grade?' + myGrade)}
+    else {
+        myGrade = prompt('No wrong try one more time');
+
+    }
 }
+console.log('what is my high school grade?' + myGrade)
+alert('My grade in hugh school is 85')
 
-let qus4 = prompt('Q4-Can I provide any help for you?');
-console.log('Can I provide any help for you? :'+ qus4)
-switch (qus4.toLowerCase()) {
+let mycollege=Number (prompt('what is my college  grade?'))
+console.log('what is my college grade?' + mycollege)
 
-    case 'yes':
-        document.write('<h1>' + 'Q4-Can I provide any help for you?' + '  ' + 'yes'+'</h1>')
-        break;
-    case 'no':
-        document.write('<h1>' + 'Q4-Can I provide any help for you?' + '  ' + 'no'+'</h1>')
-        break;
-    default:
-        document.write('<h1>' + 'Q4-'+ 'Sorry' + ' ' + userName + '  ' + ' , you did not answer well, please only answer with yes or no')
-        alert('please only answer with yes or no')
-        break;
+for (let i = 0; i < 3; i++) {
+
+    while (!mycollege) { alert('Please Enter a number'), mycollege = Number(prompt('what is my college  grade?, write only numbers'))
+    ,console.log('what is my college  grade?' + mycollege) }
+
+    if (mycollege < 50) {
+        alert('Too low for me')
+        mycollege = Number(prompt('what is my college  grade?'))
+        console.log('what is my college  grade?' + mycollege)
+    } else if (mycollege > 95) {
+        alert('More than what I desirve')
+        mycollege = Number(prompt('what is my college  grade?'))
+        console.log('what is my college  grade?' + mycollege)
+    }
+    else if (mycollege == 85) { alert('Great this is the correct answer') 
+    console.log('what is my college  grade?' + mycollege)}
+    else {
+        mycollege = prompt('No wrong try one more time');
+
+    }
 }
+console.log('what is my college  grade?' + mycollege)
+alert('My coolege grade is 62')
 
-let qus5 = prompt('Q5-Did you find my websit helpfull?');
-console.log('Did you find my websit helpfull?:'+ qus5)
-switch (qus5.toLowerCase()) {
+let gradYear=Number (prompt('what year I graduated from college?'))
+console.log('what year I graduated from college?' + gradYear)
 
-    case 'yes':
-        document.write('<h1>'+'Q5-Did you find my websit helpfull?' + '  ' + 'yes'+'</h1>')
-        break;
-    case 'no':
-        document.write('<h1>'+'Q5-Did you find my websit helpfull?' + '  ' + 'no'+'</h1>')
-        break;
-    default:
-        document.write('<h1>'+'Q5-'+'Sorry' + ' ' + userName + '  ' + ' , you did not answer well, please only answer with yes or no')
-        alert('please only answer with yes or no')
-        break;
+for (let i = 0; i < 3; i++) {
+
+    while (!gradYear) { alert('Please Enter a number'), gradYear = Number(prompt('what year I graduated from college?, write only numbers'))
+    ,console.log('what year I graduated from college?' + gradYear) }
+
+    if (gradYear < 2000) {
+        alert('Too early for me to graduate')
+        gradYear = Number(prompt('what year I graduated from college?'))
+        console.log('what year I graduated from college?' + gradYear)
+    } else if (gradYear > 2020) {
+        alert('Too close for me to graduate')
+        gradYear = Number(prompt('what year I graduated from college?'))
+        console.log('what year I graduated from college?' + gradYear)
+    }
+    else if (gradYear == 2017) { alert('Great this is the correct answer') 
+    console.log('what year I graduated from college?' + gradYear)}
+    else {
+        gradYear = prompt('No wrong try one more time');
+
+    }
 }
-alert('Great'+' '+ userName +' ' + 'your answers are shown in the page please inbox me by pressing contact, thank you')
+console.log('what year I graduated from college?' + gradYear)
+alert('I graduated 2017')
+
+let brothersNum=Number (prompt('How many brothers I have'))
+console.log('How many brothers I have?' + brothersNum)
+
+for (let i = 0; i < 3; i++) {
+
+    while (!brothersNum) { alert('Please Enter a number'), brothersNum = Number(prompt('How many brothers I have, write only numbers'))
+    ,console.log('How many brothers I have' + brothersNum) }
+
+    if (brothersNum > 10) {
+        alert('Too larg for my family')
+        brothersNum = Number(prompt('How many brothers I have'))
+        console.log('How many brothers I have' + brothersNum)
+    } else if (brothersNum < 4) {
+        alert('Too small for my family')
+        brothersNum = Number(prompt('How many brothers I have'))
+        console.log('How many brothers I have' + brothersNum)
+    }
+    else if (brothersNum == 6) { alert('Great this is the correct answer') 
+    console.log('How many brothers I have' + brothersNum)}
+    else {
+        brothersNum = prompt('No wrong try one more time');
+
+    }
+}
+console.log('How many brothers I have' + gradYear)
+alert('My brotherss number is 6 ')*/
+
+
+/*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
+
+/*let qus1=prompt('Who is your favourite student in 201 class')
+console.log('Who is your favourite student in 201 class' + qus1 )*/
+
+
+let qus1 = prompt('Who is your favourite student in 201 class')
+
+let qus1 = ['ahmad', 'mohammad', 'kaled', 'hasan', 'hanan', 'husam', 'suahib', 'ola', 'reem', 'ro\'a', 'leen', ' rand', 'reham']
+
+for (let i = 0; i < 6; i++) {
+
+    if (qus1 == 'indexe.lengh') {
+        alert('You answered correctly'), console.log('Who is your favourite student in 201 class' + qus1)
+    
+    } 
+    else if (qus1 !== 'indexe.lengh') {
+        alert('You\'r gues is wrong , try again please'), qus1 = prompt('Who is your favourite student in 201 class?')
+    
+    }
+    else {alert('you\rr answer is wrong')
+    
+    } 
+    
+    
+    
+    }
+
+
+/*
+
+switch (qus1.toLocaleLowerCase()) {
+        case 'qus': prompt('great you answered croectly')
+
+            break;
+        case '!qus': alert('You\'r answer is wrong')
+
+        default:prompt('try again')
+            break;
+    }
+}
+*/
