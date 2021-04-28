3947a369effb33ab691b8395a5b2a2306d5e5a6c
khaled611993 <khaledfa.93@gmail.com>
2021-04-28 14:43:30 +0300

    from khaled

--- js/app.js
+++ js/app.js
@@ -1,17 +1,19 @@
 ' use strict';
-/*alert('Hello we are going to play a gues game')
 
+
+function firstOne(){
+alert('Hello we are going to play a guess game')
 let myOld = Number(prompt('How old I\'m I ?'))
 
 
 
 
 
-for (let i = 0; i < 3; i++) {
-
+for (let i = 0; i <= 3; i++) {
+    
     while (!myOld) { alert('Please Enter a number'), myOld = Number(prompt('How old I\'m I ?, writ only numbers'))
     ,console.log('How old I\'m I ?' + myOld) }
-
+    
     if (myOld < 20) {
         alert('Too Low')
         myOld = Number(prompt('How old I\'m I ?'))
@@ -22,27 +24,30 @@ for (let i = 0; i < 3; i++) {
         console.log('How old I\'m I ?' + myOld)
     }
     else if (myOld == 29) { alert('Great this is the correct answer') 
-    console.log('How old I\'m I ?' + myOld)}
+    console.log('How old I\'m I ?' + myOld)
+break;
+}
     else {
         myOld = prompt('No wrong try one more time');
-
+        
     }
 }
 console.log('How old I\'m I ?' + myOld)
-alert('My old is 29, whish me a happy life')
+alert('My old is 29, wish me a happy life')
 
+}
+  firstOne();
 
+  function secondOne(){
+      let birthYear=Number (prompt('What year I was porn?'))
+      console.log('How old I\'m I ?' + birthYear)
 
-
-let birthYear=Number (prompt('What year I was porn?'))
-console.log('How old I\'m I ?' + birthYear)
-
-
-for (let i = 0; i < 3; i++) {
-
+      
+      for (let i = 0; i < 3; i++) {
+          
     while (!birthYear) { alert('Please Enter a number'), birthYear = Number(prompt('What year I was porn?, writ only numbers'))
     ,console.log('What year I was porn?' + birthYear) }
-
+    
     if (birthYear < 1985) {
         alert('Too old for that')
         birthYear = Number(prompt('What year I was porn?'))
@@ -53,72 +58,80 @@ for (let i = 0; i < 3; i++) {
         console.log('What year I was porn?' + birthYear)
     }
     else if (birthYear == 1993) { alert('Great this is the correct answer') 
+    break;
     console.log('What year I was porn?' + birthYear)}
     else {
         birthYear = prompt('No wrong try one more time');
-
+        
     }
 }
-console.log('How old I\'m I ?' + myOld)
+console.log('How old I\'m I ?' + birthYear)
 alert('I porned in 1993, whish me a happy life')
-
-
-
-let myGrade=Number (prompt('what is my high school grade?'))
-console.log('what is my high school grade?' + myGrade)
-
-
-for (let i = 0; i < 3; i++) {
-
-    while (!myGrade) { alert('Please Enter a number'), myGrade = Number(prompt('what is my high school grade?, write only numbers'))
-    ,console.log('what is my high school grade?' + myGrade) }
-
-    if (myGrade < 50) {
-        alert('Too low for me')
-        myGrade = Number(prompt('what is my high school grade?'))
-        console.log('what is my high school grade?' + myGrade)
-    } else if (myGrade > 95) {
-        alert('More than what I desirve')
-        myGrade = Number(prompt('what is my high school grade?'))
-        console.log('what is my high school grade?' + myGrade)
-    }
-    else if (myGrade == 85) { alert('Great this is the correct answer') 
-    console.log('what is my high school grade?' + myGrade)}
-    else {
-        myGrade = prompt('No wrong try one more time');
-
-    }
 }
-console.log('what is my high school grade?' + myGrade)
-alert('My grade in hugh school is 85')
-
-let mycollege=Number (prompt('what is my college  grade?'))
-console.log('what is my college grade?' + mycollege)
-
-for (let i = 0; i < 3; i++) {
-
-    while (!mycollege) { alert('Please Enter a number'), mycollege = Number(prompt('what is my college  grade?, write only numbers'))
-    ,console.log('what is my college  grade?' + mycollege) }
-
-    if (mycollege < 50) {
-        alert('Too low for me')
-        mycollege = Number(prompt('what is my college  grade?'))
-        console.log('what is my college  grade?' + mycollege)
-    } else if (mycollege > 95) {
-        alert('More than what I desirve')
-        mycollege = Number(prompt('what is my college  grade?'))
-        console.log('what is my college  grade?' + mycollege)
+secondOne();
+function therrdOne(){
+    let myGrade=Number (prompt('what is my high school grade?'))
+    console.log('what is my high school grade?' + myGrade)
+    
+    
+    for (let i = 0; i < 3; i++) {
+        
+        while (!myGrade) { alert('Please Enter a number'), myGrade = Number(prompt('what is my high school grade?, write only numbers'))
+        ,console.log('what is my high school grade?' + myGrade) }
+        
+        if (myGrade < 50) {
+            alert('Too low for me')
+            myGrade = Number(prompt('what is my high school grade?'))
+            console.log('what is my high school grade?' + myGrade)
+        } else if (myGrade > 95) {
+            alert('More than what I desirve')
+            myGrade = Number(prompt('what is my high school grade?'))
+            console.log('what is my high school grade?' + myGrade)
+        }
+        else if (myGrade == 85) { alert('Great this is the correct answer') 
+        break;
+        console.log('what is my high school grade?' + myGrade)}
+        else {
+            myGrade = prompt('No wrong try one more time');
+            
+        }
     }
-    else if (mycollege == 85) { alert('Great this is the correct answer') 
-    console.log('what is my college  grade?' + mycollege)}
-    else {
-        mycollege = prompt('No wrong try one more time');
-
+    console.log('what is my high school grade?' + myGrade)
+    alert('My grade in hugh school is 85')
+}
+therrdOne();
+function fourthOne(){
+    let mycollege=Number (prompt('what is my college  grade?'))
+    console.log('what is my college grade?' + mycollege)
+    
+    for (let i = 0; i < 3; i++) {
+        
+        while (!mycollege) { alert('Please Enter a number'), mycollege = Number(prompt('what is my college  grade?, write only numbers'))
+        ,console.log('what is my college  grade?' + mycollege) }
+        
+        if (mycollege < 50) {
+            alert('Too low for me')
+            mycollege = Number(prompt('what is my college  grade?'))
+            console.log('what is my college  grade?' + mycollege)
+        } else if (mycollege > 95) {
+            alert('More than what I desirve')
+            mycollege = Number(prompt('what is my college  grade?'))
+            console.log('what is my college  grade?' + mycollege)
+        }
+        else if (mycollege == 85) { alert('Great this is the correct answer') 
+        break;
+        console.log('what is my college  grade?' + mycollege)}
+        else {
+            mycollege = prompt('No wrong try one more time');
+            
     }
 }
 console.log('what is my college  grade?' + mycollege)
-alert('My coolege grade is 62')
+alert('My coolege grade is 85')
+}
+fourthOne();
 
+function fivethOne(){
 let gradYear=Number (prompt('what year I graduated from college?'))
 console.log('what year I graduated from college?' + gradYear)
 
@@ -137,6 +150,7 @@ for (let i = 0; i < 3; i++) {
         console.log('what year I graduated from college?' + gradYear)
     }
     else if (gradYear == 2017) { alert('Great this is the correct answer') 
+    break;
     console.log('what year I graduated from college?' + gradYear)}
     else {
         gradYear = prompt('No wrong try one more time');
@@ -150,10 +164,10 @@ let brothersNum=Number (prompt('How many brothers I have'))
 console.log('How many brothers I have?' + brothersNum)
 
 for (let i = 0; i < 3; i++) {
-
+    
     while (!brothersNum) { alert('Please Enter a number'), brothersNum = Number(prompt('How many brothers I have, write only numbers'))
     ,console.log('How many brothers I have' + brothersNum) }
-
+    
     if (brothersNum > 10) {
         alert('Too larg for my family')
         brothersNum = Number(prompt('How many brothers I have'))
@@ -164,45 +178,55 @@ for (let i = 0; i < 3; i++) {
         console.log('How many brothers I have' + brothersNum)
     }
     else if (brothersNum == 6) { alert('Great this is the correct answer') 
+    break;
     console.log('How many brothers I have' + brothersNum)}
     else {
         brothersNum = prompt('No wrong try one more time');
-
+        
     }
 }
 console.log('How many brothers I have' + gradYear)
-alert('My brotherss number is 6 ')*/
-
+alert('My brothers number is 6 ')
+}
+fivethOne();
 
 /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
 
 /*let qus1=prompt('Who is your favourite student in 201 class')
-console.log('Who is your favourite student in 201 class' + qus1 )*/
+console.log('Who is your favourite student in 201 class' + qus1 )
 
 
-/*let qus = prompt('Who is your favourite student in 201 class')*/
+
+let qus = prompt('Who is your favourite student in 201 class')
 let score = 0
-let qus = ['ahmad', 'mohammad', 'kaled', 'hasan', 'hanan', 'husam']
+
+let qus = ['ahmad', 'mohammad', 'khaled', 'hasan', 'hanan', 'husam']
 
 for (let k = 0; k <= 5; k++) {
     let qus1 = prompt('Who is your favourite student in 201 class')
-
-    for (let i = 0; i < qus.length; i++) {
-
+    
+    for (let e = 0; e < qus.length; e++) {
+        
         if (qus1 == qus[i]) {
             alert('This is a correct answer')
             score++
             k = 6
             break;
         } 
-
+        
     }
-    if (qus1 !== qus[i]) alert('your answer is wrong you have only 6 attemots')
-        break;
+    */
+    
+ /*
+  if(!qus[i]){
+      alert('your answer is wrong')
+  }
+            
 }
 alert('All correct possible answer is: ahmad mohammad khaled hassan hanan husam ')
 
 
+*/
 
 
 
@@ -214,8 +238,8 @@ alert('All correct possible answer is: ahmad mohammad khaled hassan hanan husam 
 
 
 
-
-/*switch (qus.toLocaleLowerCase()) {
+/*
+switch (qus.toLocaleLowerCase()) {
     case 'qus[i]': prompt('great you answered croectly')
 
         break;
@@ -239,8 +263,9 @@ alert('All correct possible answer is: ahmad mohammad khaled hassan hanan husam 
 
 alert('The corect answers is');
 
-
-/* switch (ansWer) {
+*/
+/*
+ switch (ansWer) {
         case qus[i]:
             alert(' Great You\'r answer is correct')
 
@@ -249,7 +274,8 @@ alert('The corect answers is');
         default: alert('Your answer is incorect please try again'),
             ansWer = prompt('Who is your favourite student in 201 class')
             break;
-    }*/
+    }
+    */
 
 
 
@@ -270,7 +296,8 @@ alert('The corect answers is');
 
 
 
-/*if (qus[i] == ansWer) {
+/*
+if (qus[i] == ansWer) {
     alert(' Great You\'r answer is correct')
     break;
 
@@ -280,7 +307,8 @@ alert('The corect answers is');
     ansWer = prompt('Who is your favourite student in 201 class')
 
 
-}*/
+}
+*/
 
 
 
@@ -323,7 +351,8 @@ for (let i = 0; i < qus.length; i++) {
 
 
 
-}*/
+}
+*/
 
 
 /*
@@ -337,7 +366,8 @@ switch (qus1.toLocaleLowerCase()) {
         default:prompt('try again')
             break;
     }
-}*/
+}
+*/
 
 
 
