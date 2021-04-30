5591eb7bd27e9c328270550307893e63fc1a6a41
MohannadAteyyat <ateyyatmohanad@gmail.com>
2021-04-30 21:25:01 +0300

    After Grade

--- js/class02a.js
+++ js/class02a.js
@@ -4,14 +4,18 @@ console.log('uSERnAME :'+ userName)
 
 alert('Welcome to my humble page'+' '+ userName +' '+ 'please answer the following questions')
 
-let qus1 = prompt( 'Q1-Are you a software developer?')
-console.log('Are you a software developer? :'+ qus1)
+let qus1 = prompt( 'Q1-Do you think that im a software developer?')
+console.log('Do you think that im a software developer? :'+ qus1)
 switch (qus1.toLowerCase()) {
     case 'yes':
-        document.write('<h1>' + 'Q1-Are you a software developer?' + '  ' + 'yes'+'</h1>')
+        case 'y':
+            alert('You answered corectly well done.')
+        document.write('<h1>' + 'Q1-Do you think that im a software developer?' + '  ' + 'yes'+'</h1>')
         break;
     case 'no':
-        document.write('<h1>' + 'Q1-Are you a software developer?' + '  ' + 'no'+'</h1>')
+        case 'n':
+            alert('Sorry You\'r answer is wrong keep trying.')
+        document.write('<h1>' + 'Do you think that im a software developer?' + '  ' + 'no'+'</h1>')
         break;
     default:
         document.write('<h1>' + 'Q1-'+'Sorry' + ' ' + userName + '  ' + ' , you did not answer well, please only answer with yes or no')
@@ -19,14 +23,18 @@ switch (qus1.toLowerCase()) {
         break;
 }
 
-let qus2 = prompt('Q2-Are you looking for software developer?');
-console.log('Are you looking for software developer? :'+ qus2)
+let qus2 = prompt('Q2-Do you think that I can do it with this course?');
+console.log('Do you think that I can do it with this course? :'+ qus2)
 switch (qus2.toLowerCase()) {
     case 'yes':
-        document.write('<h1>' + 'Q2-Are you looking for software developer?' + '  ' + 'yes'+'</h1>')
+        case 'y':
+            alert('You answered corectly well done.')
+        document.write('<h1>' + 'Q2-Do you think that I can do it with this course?' + '  ' + 'yes'+'</h1>')
         break;
-    case 'no':
-        document.write('<h1>' + 'Q2-Are you looking for software developer?' + '  ' + 'no'+'</h1>')
+        case 'no':
+            case 'n':
+                alert('Sorry You\'r answer is wrong keep trying.')
+        document.write('<h1>' + 'Q2-Do you think that I can do it with this course?' + '  ' + 'no'+'</h1>')
         break;
     default:
         document.write('<h1>' + 'Q2-'+'Sorry' + ' ' + userName + '  ' + ' , you did not answer well, please only answer with yes or no')
@@ -34,15 +42,21 @@ switch (qus2.toLowerCase()) {
         break;
 }
 
-let qus3 = prompt('Q3-Did you liked my website?');
-console.log('Did you liked my website? :'+ qus3)
+let qus3 = prompt('Q3-Do you think that I can understand what Bra\'ah saying?');
+console.log('Do you think that I can understand what Bra\'ah saying? :'+ qus3)
 switch (qus3.toLowerCase()) {
 
     case 'yes':
-        document.write('<h1>' + 'Q3-Did you liked my website?' + '  ' + 'yes'+'</h1>')
+        case 'y':
+      alert('Sorry You\'r answer is wrong keep trying.')
+        document.write('<h1>' + 'Q3-Do you think that I can understand what Bra\'ah saying?' + '  ' + 'no'+'</h1>')
+
         break;
-    case 'no':
-        document.write('<h1>' + 'Q3-Did you liked my website?' + '  ' + 'no'+'</h1>')
+        case 'no':
+            case 'n':
+                
+        alert('You answered corectly well done.')
+        document.write('<h1>' + 'Q3-Do you think that I can understand what Bra\'ah saying?' + '  ' + 'yes'+'</h1>')
         break;
     default:
         document.write('<h1>' + 'Q3-'+'Sorry' + ' ' + userName + '  ' + ' , you did not answer well, please only answer with yes or no')
@@ -50,15 +64,19 @@ switch (qus3.toLowerCase()) {
         break;
 }
 
-let qus4 = prompt('Q4-Can I provide any help for you?');
-console.log('Can I provide any help for you? :'+ qus4)
+let qus4 = prompt('Q4-Can you provide help to me?');
+console.log('Can you provide help to me? :'+ qus4)
 switch (qus4.toLowerCase()) {
 
     case 'yes':
-        document.write('<h1>' + 'Q4-Can I provide any help for you?' + '  ' + 'yes'+'</h1>')
+        case 'y':
+            alert('You answered corectly well done.')
+        document.write('<h1>' + 'Q4-Can you provide help to me?' + '  ' + 'yes'+'</h1>')
         break;
-    case 'no':
-        document.write('<h1>' + 'Q4-Can I provide any help for you?' + '  ' + 'no'+'</h1>')
+        case 'no':
+            case 'n':
+                alert('Sorry You\'r answer is wrong keep trying.')
+        document.write('<h1>' + 'Q4-Can you provide help to me?' + '  ' + 'no'+'</h1>')
         break;
     default:
         document.write('<h1>' + 'Q4-'+ 'Sorry' + ' ' + userName + '  ' + ' , you did not answer well, please only answer with yes or no')
@@ -66,15 +84,19 @@ switch (qus4.toLowerCase()) {
         break;
 }
 
-let qus5 = prompt('Q5-Did you find my websit helpfull?');
-console.log('Did you find my websit helpfull?:'+ qus5)
+let qus5 = prompt('Q5-Do I live in Jordan?');
+console.log('Do I live in Jordan?:'+ qus5)
 switch (qus5.toLowerCase()) {
 
     case 'yes':
-        document.write('<h1>'+'Q5-Did you find my websit helpfull?' + '  ' + 'yes'+'</h1>')
+        case 'y':
+            alert('You answered corectly well done.')
+        document.write('<h1>'+'Q5-Do I live in Jordan?' + '  ' + 'yes'+'</h1>')
         break;
-    case 'no':
-        document.write('<h1>'+'Q5-Did you find my websit helpfull?' + '  ' + 'no'+'</h1>')
+        case 'no':
+            case 'n':
+                alert('Sorry You\'r answer is wrong keep trying.')
+        document.write('<h1>'+'Q5-Do I live in Jordan?' + '  ' + 'no'+'</h1>')
         break;
     default:
         document.write('<h1>'+'Q5-'+'Sorry' + ' ' + userName + '  ' + ' , you did not answer well, please only answer with yes or no')
