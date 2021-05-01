528b4a1d46f332866912e0117863c437d3dc4ce0
MohannadAteyyat <ateyyatmohanad@gmail.com>
2021-05-01 13:30:15 +0300

    Lab03 re-submitting assignment

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 ' use strict';
 let userName = prompt('Hello, could you please type your name?')
+alert('Hello' +' ' + userName + ' we are going to play a guess game')
 console.log('uSERnAME :'+ userName)
+
+let score = 0
 
 alert('Welcome to my humble page'+' '+ userName +' '+ 'please answer the following questions')
 
@@ -11,6 +14,7 @@ switch (qus1.toLowerCase()) {
         case 'y':
             alert('You answered corectly well done.')
         document.write('<h1>' + 'Q1-Do you think that im a software developer?' + '  ' + 'yes'+'</h1>')
+        score++
         break;
     case 'no':
         case 'n':
@@ -30,6 +34,7 @@ switch (qus2.toLowerCase()) {
         case 'y':
             alert('You answered corectly well done.')
         document.write('<h1>' + 'Q2-Do you think that I can do it with this course?' + '  ' + 'yes'+'</h1>')
+        score++
         break;
         case 'no':
             case 'n':
@@ -57,6 +62,7 @@ switch (qus3.toLowerCase()) {
                 
         alert('You answered corectly well done.')
         document.write('<h1>' + 'Q3-Do you think that I can understand what Bra\'ah saying?' + '  ' + 'yes'+'</h1>')
+        score++
         break;
     default:
         document.write('<h1>' + 'Q3-'+'Sorry' + ' ' + userName + '  ' + ' , you did not answer well, please only answer with yes or no')
@@ -72,6 +78,7 @@ switch (qus4.toLowerCase()) {
         case 'y':
             alert('You answered corectly well done.')
         document.write('<h1>' + 'Q4-Can you provide help to me?' + '  ' + 'yes'+'</h1>')
+        score++
         break;
         case 'no':
             case 'n':
@@ -92,6 +99,7 @@ switch (qus5.toLowerCase()) {
         case 'y':
             alert('You answered corectly well done.')
         document.write('<h1>'+'Q5-Do I live in Jordan?' + '  ' + 'yes'+'</h1>')
+        score++
         break;
         case 'no':
             case 'n':
@@ -104,3 +112,73 @@ switch (qus5.toLowerCase()) {
         break;
 }
 alert('Great'+' '+ userName +' ' + 'your answers are shown in the page please inbox me by pressing contact, thank you')
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+function firstOne() {
+    
+    let myOld = Number(prompt('How old I\'m I ?'))
+
+    for (let i = 0; i <= 3; i++) {
+
+        while (!myOld) {
+            alert('Please Enter a number'), myOld = Number(prompt('How old I\'m I ?, writ only numbers'))
+                , console.log('How old I\'m I ?' + myOld)
+        }
+
+        if (myOld < 20) {
+            alert('Too Low')
+            myOld = Number(prompt('How old I\'m I ?'))
+            console.log('How old I\'m I ?' + myOld)
+        } else if (myOld > 35) {
+            alert('Too High')
+            myOld = Number(prompt('How old I\'m I ?'))
+            console.log('How old I\'m I ?' + myOld)
+        }
+        else if (myOld == 29) {
+            alert('Great this is the correct answer')
+            score++
+            console.log('How old I\'m I ?' + myOld)
+            break;
+        }
+        else {
+            myOld = prompt('No wrong try one more time');
+
+        }
+    }
+    console.log('How old I\'m I ?' + myOld)
+    alert('My old is 29, wish me a happy life')
+
+}
+firstOne();
+
+
+//  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+
+let qus = ['ahmad', 'mohammad', 'kaled', 'hasan', 'hanan', 'husam', 'suahib', 'ola', 'reem', 'ro\'a', 'leen', ' rand', 'reham']
+for (let j = 0; j < 6; j++) {
+    let qus1 = prompt('Who is my favourite student in 201 class', 'ahmad, mohammad, kaled, hasan, hanan, husam, suahib, ola, reem, ro\'a, leen,  rand, reham')
+    console.log('Who is my favourite student in 201 class' + qus1)
+    for (let i = 0; i <= qus.length; i++) {
+        if (qus1 === qus[i]) {
+            alert('You answered correctly'), console.log('Who is my favourite student in 201 class' + qus1)
+            j = 7
+            score++
+            break;
+        }
+    }
+   
+        if (j<7) {
+            alert('You\'r gues is wrong , try again please'), console.log('Who is my favourite student in 201 class' + qus1)
+            
+        }
+    
+}
+
+alert('Yor score is :' + ' ' + score)
